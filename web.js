a7638e63dbb22f42b34b7f5e0909619559222021
Stoyan Zhekov <zh@zhware.net>
2013-07-07 18:33:04 +0900

    Added index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
-var express = require('express');
+var express = require('express'),
+    fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fs.readFileSync('./index.html','utf-8'));
 });
 
 var port = process.env.PORT || 5000;
